5c82ff455f66dcf721fd4403d9638c8f16639a45
Rezishon <rezishon@duck.com>
2024-04-03 14:44:06 +0330

    feat: set styles
    The style object added to tags style part

--- src/Header.js
+++ src/Header.js
@@ -2,16 +2,17 @@ import React from "react";
 
 function Header() {
   return (
-    <div>
-      <label>صفحه اصلی</label>
-      <label>درباره سایت</label>
-      <label>قوانین و مقررات سایت</label>
-      <label>تماس با ما</label>
+    <div style={Style.div_style}>
+      <label style={Style.Label_style}>صفحه اصلی</label>
+      <label style={Style.Label_style}>درباره سایت</label>
+      <label style={Style.Label_style}>قوانین و مقررات سایت</label>
+      <label style={Style.Label_style}>تماس با ما</label>
     </div>
   );
 }
 
 export default Header;
+
 let Style = {
   Label_style: {
     backgroundColor: "greenyellow",
