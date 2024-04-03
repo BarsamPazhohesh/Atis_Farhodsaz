1151e6109959a1c4cd3c95de9127c27e293e4922
Rezishon <rezishon@duck.com>
2024-04-03 14:39:38 +0330

    feat: add Style
    The style added in object format

--- src/Header.js
+++ src/Header.js
@@ -12,3 +12,12 @@ function Header() {
 }
 
 export default Header;
+let Style = {
+  Label_style: {
+    backgroundColor: "greenyellow",
+    padding: "10px",
+    borderRadius: "10px",
+    // with:"10vh"
+    margin: "0px 10px 0px 10px",
+  },
+};
