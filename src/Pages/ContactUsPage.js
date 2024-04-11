0982d061a73019ff6e05fe5f9d08a0d5c019a550
Rezishon <rezishon@duck.com>
2024-04-11 05:20:03 +0330

    fix: remove repeated codes
    Because the main parts of each pages was same as others os I created a
    component which will create each page with their ditails. It needs text
    in array format and the page name we are using it in

--- src/Pages/ContactUsPage.js
+++ src/Pages/ContactUsPage.js
@@ -1,37 +1,20 @@
 //#region Dependencies
 import React from "react";
-import Footer from "../Footer";
-import Header from "../Header";
-import { Style } from "./MainPage";
-import topImage from "./Header4.jpg";
-
+import Page from "./Page";
 //#endregion
 
 //#region ContactUsPage function
 function ContactUsPage() {
-  return (
-    <div style={Style.main_style}>
-      <Header />
-      <div style={{ marginTop: "10px" }} class="w3-container">
-        <img src={topImage} class="w3-round" alt="Norway" />
-        <h6 style={{ fontSize: "3vh" }}>
-          روش های تماس با مدیریت و کارشناسان سامانه آتیس فرهود
-        </h6>
-        <h6 style={{ fontSize: "3vh" }}>سامانه آتیس فرهود</h6>
-        <h6 style={{ fontSize: "3vh" }}>مدیریت سامانه : 03133870111</h6>
-        <h6 style={{ fontSize: "3vh" }}>دفتر شکایات : 03133870111</h6>
-        <h6 style={{ fontSize: "3vh" }}>کارشناس فنی : 03133870111</h6>
-        <h6 style={{ fontSize: "3vh" }}>
-          پشتیبانی : 03133863070 و 03133869114
-        </h6>
-        <h6 style={{ fontSize: "3vh" }}>
-          استان: اصفهان، شهرستان اصفهان،بخش مرکزی،خ امام خمینی ،خ امیرکبیر،خ
-          قائم مقام جنوبی ،خ پرتو، ترمینال ، ساختمان اداری ،{" "}
-        </h6>
-      </div>
-      <Footer />
-    </div>
-  );
+  let pageText = [
+    "روش های تماس با مدیریت و کارشناسان سامانه آتیس فرهود",
+    "مدیریت سامانه : 03133870111",
+    "دفتر شکایات : 03133870111",
+    "کارشناس فنی : 03133870111",
+    "پشتیبانی : 03133863070 و 03133869114",
+    "استان: اصفهان، شهرستان اصفهان،بخش مرکزی،خ امام خمینی ،خ امیرکبیر،خ قائم مقام جنوبی ،خ پرتو، ترمینال ، ساختمان اداری",
+  ];
+
+  return <Page text={pageText} pageName={"contactUsPage"} />;
 }
 //#endregion
 
