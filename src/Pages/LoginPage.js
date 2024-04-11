1130978a32e0d86ad06de833a7fc007df0da074a
Rezishon <rezishon@duck.com>
2024-04-11 21:51:46 +0330

    feat: add main div
    This div will handle main components of page which are header, footer &
    middle part. The justify part added to place footer to its place in
    screen

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -2,6 +2,15 @@ import React from "react";
 
 function LoginPage() {
   return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        height: "97vh",
+      }}
+    >
+    </div>
   );
 }
 
