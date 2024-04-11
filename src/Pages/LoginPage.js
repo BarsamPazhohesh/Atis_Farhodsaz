22f7639f713bcc69d4a01f190a8c0a1c6e9443f2
Rezishon <rezishon@duck.com>
2024-04-11 22:00:33 +0330

    feat: add styling
    Because i use w3.css as framework for css, I used too much class names
    os repalced them into a object. Now the code is more readable and clear

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -3,6 +3,12 @@ import Header from "../Header";
 import Footer from "../Footer";
 
 function LoginPage() {
+  let ClassNames = {
+    middleDiv: "w3-container w3-half w3-margin-bottom w3-mobile",
+    theForm:
+      "w3-container w3-border w3-border-black  w3-pale-yellow w3-card-4 w3-round-xlarge w3-mobile",
+  };
+
   return (
     <div
       style={{
