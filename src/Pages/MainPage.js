46bed190a45ad6774996cf5d77b2da179e1007a7
Rezishon <rezishon@duck.com>
2024-04-12 23:40:31 +0330

    fix: remove old codes
    These lines will be replaced with better codes. This part written one
    time but I accidently losed it so it's the second time I'm writting it

--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -9,33 +9,11 @@ import topImage from "./Header4.jpg";
 //#region MainPage function
 function MainPage() {
   return (
-    <>
-      <div style={Style.main_style} class="w3-display-container w3-mobile">
-        <Header />
-        <div style={{ marginTop: "10px" }} class="w3-container">
-          <img src={topImage} class="w3-round" alt="Norway" />
-          <p style={{ fontSize: "3vh" }}>
-            کلیه حقوق سایت متعلق است به اداره کل راهداری و حمل و نقل جاده ای
-            استان اصفهان
           </p>
         </div>
-        <Footer />
       </div>
-    </>
   );
 }
 //#endregion
 
 export default MainPage;
-
-//#region Style object
-export let Style = {
-  main_style: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "space-between",
-    // height: "100%",
-    textAlign: "center",
-  },
-};
-//#endregion
