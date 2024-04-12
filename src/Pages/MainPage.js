f1ebcebf9a44d5a57118cac1d5a9755f7bbb9089
Rezishon <rezishon@duck.com>
2024-04-12 23:45:30 +0330

    feat: add Persian texts
    This div has 2 parts which are has both custom styling & w3.css classes

--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -18,6 +18,20 @@ function MainPage() {
       <Header />
 
       <div>
+        <div className="w3-right w3-mobile w3-container">
+          <p
+            style={{ margin: "0px", fontWeight: "bold" }}
+            className="w3-center w3-xxxlarge"
+          >
+            سامانه آتیس
+          </p>
+          <p
+            style={{ margin: "0px" }}
+            className="w3-center w3-margin-top w3-xxlarge"
+          >
+            ارائه خدمات الکترونیک و اطلاع رسانی بار
+          </p>
+        </div>
           </p>
         </div>
       </div>
