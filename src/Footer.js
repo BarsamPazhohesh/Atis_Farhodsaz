e93defe49ba730892568e746c86ebe5c43c90f4b
Rezishon <rezashahmoradi.81@outlook.com>
2024-04-14 22:52:37 -0400

    fix: remove additional divs
    Due to needs of cutomer, only E Namad should be shown on website so I
    removed additional divs of images

--- src/Footer.js
+++ src/Footer.js
@@ -5,17 +5,8 @@ import React from "react";
 //#region Footer function
 function Footer() {
   return (
-    <div style={Style.main_style}>
-      <div class="w3-card-4">
-        {/* <img src="img_snowtops.jpg" alt="Alps"> */}
-        <div class="w3-container w3-center">
-          <p>ICON</p>
-        </div>
-      </div>
-
       <div class="w3-card-4">
         <div class="w3-container w3-center">
-          <div>
             <a
               target="_blank"
               rel="noopener noreferrer"
@@ -28,14 +19,6 @@ function Footer() {
                 code="zNv5Xe9LoS7uGvf1GLLIIcDIMbY1o2Qc"
               />
             </a>
-          </div>
-        </div>
-      </div>
-
-      <div class="w3-card-4">
-        {/* <img src="img_snowtops.jpg" alt="Alps"> */}
-        <div class="w3-container w3-center">
-          <p>ICON</p>
         </div>
       </div>
 
