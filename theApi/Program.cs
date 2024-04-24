8a983792f76fcbe5a3c89525787edc2fbae30c3a
rezishon <rezashahmoradi.81@outlook.com>
2024-04-23 23:56:29 -0400

    feat: add program file

--- theApi/Program.cs
+++ theApi/Program.cs
@@ -1,7 +1,14 @@
 
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using NSwag.AspNetCore;
 
+namespace theApi;
 
+public class Program
 {
+    public static void Main(string[] args)
+    {
 
 
 
